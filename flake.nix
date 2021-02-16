fa73cf174a3d43f1a5a3ca94ae0ecd325e37e98c
Kenji <aks.kenji@protonmail.com>
2021-02-16 22:18:44 +0100

    Adding wasm32-wasi target

--- flake.nix
+++ flake.nix
@@ -1,45 +1,80 @@
 {
-  inputs = {
+description = "Zellij Environment";
+
+inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    naersk.url = "github:nmattia/naersk";
+    devshell.url = "github:numtide/devshell";
+
+    binaryen.url = "github:WebAssembly/binaryen";
+    binaryen.flake = false;
+
+    utils.url = "github:numtide/flake-utils";
+    utils.inputs.nixpkgs.follows = "nixpkgs";
 
     zellij.url = "github:zellij-org/zellij";
     zellij.flake = false;
-
-    devshell.url = "github:numtide/devshell";
-
-    rust-overlay.url = "github:oxalica/rust-overlay";
-
-    utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:nmattia/naersk";
   };
 
-  description = "A very basic flake";
-
-  outputs = { self, nixpkgs, zellij, devshell, rust-overlay, naersk, utils}:
+outputs = { self, nixpkgs, zellij, binaryen, devshell, rust-overlay, naersk, utils}:
        utils.lib.eachDefaultSystem (system: let
-        overlays = [ rust-overlay ];
-        pkgs = import "${nixpkgs}".legacyPackages"${system}" {
-        inherit overlays system;
+
+         binaryenUnstable = pkgs.stdenv.mkDerivation rec {
+           pname = "binaryen";
+           version = "99";
+           src = binaryen;
+           nativeBuildInputs = with pkgs;[ cmake python3 ];
+         };
+
+      overlays = [ (import rust-overlay) ];
+
+      pkgs = import nixpkgs {
+        inherit system overlays;
       };
+
       naersk-lib = naersk.lib."${system}";
+      RUST_BACKTRACE = 1;
+      targets = [ "wasm32-wasi" ];
+      extensions = [
+        "rust-src"
+        "rustfmt-preview"
+        "clippy-preview"
+        "rust-analysis"
+      ];
+
+  ruststable = pkgs.rust-bin.stable.latest.rust.override {
+    inherit extensions targets;
+  };
+
+    buildInputs = [
+      ruststable
+      #pkgs.rustc
+      pkgs.cargo
+      pkgs.rust-analyzer
+      #pkgs.binaryen
+      #pkgs.wasm-pack
+      binaryenUnstable
+    ];
+
   in
     rec {
         # `nix build`
-      packages.my-project = naersk-lib.buildPackage {
-        pname = "my-project";
+      packages.zellij = naersk-lib.buildPackage {
+        pname = "zellij";
         root = zellij;
       };
-      defaultPackage = packages.my-project;
+      defaultPackage = packages.zellij;
 
       # `nix run`
-      apps.my-project = utils.lib.mkApp {
-        drv = packages.my-project;
+      apps.zellij = utils.lib.mkApp {
+        drv = packages.zellij;
       };
-      defaultApp = apps.my-project;
+      defaultApp = apps.zellij;
 
       # `nix develop`
       devShell = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [ rustc cargo ];
+        inherit  buildInputs RUST_BACKTRACE;
       };
     });
 }
