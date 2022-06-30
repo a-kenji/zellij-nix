a91605e783321e1f8b69d64ec303e5bc0f3cfbcc
a-kenji <aks.kenji@protonmail.com>
2022-06-30 16:36:38 +0200

    chore(fmt): treefmt

--- nix/default.nix
+++ nix/default.nix
@@ -11,78 +11,78 @@
 # flake outputs
 
 flake-utils.lib.eachDefaultSystem (system:
-  let
+let
 
-    binaryenUnstable = pkgs.callPackage ./binaryen.nix { inherit binaryen; };
+  binaryenUnstable = pkgs.callPackage ./binaryen.nix { inherit binaryen; };
 
-    overlays = [ (import rust-overlay) ];
+  overlays = [ (import rust-overlay) ];
 
-    pkgs = import nixpkgs {
-      inherit system overlays;
-    };
+  pkgs = import nixpkgs {
+    inherit system overlays;
+  };
 
-    # The root directory of this project
-    ZELLIJ_ROOT = toString ./.;
-    # Set up a local directory to install binaries in
-    CARGO_INSTALL_ROOT = "${ZELLIJ_ROOT}/.cargo";
+  # The root directory of this project
+  ZELLIJ_ROOT = toString ./.;
+  # Set up a local directory to install binaries in
+  CARGO_INSTALL_ROOT = "${ZELLIJ_ROOT}/.cargo";
 
-    rustToolchainToml = pkgs.rust-bin.fromRustupToolchainFile (zellij + /rust-toolchain.toml);
-    cargoLockFile = (zellij + /Cargo.lock);
+  rustToolchainToml = pkgs.rust-bin.fromRustupToolchainFile (zellij + /rust-toolchain.toml);
+  cargoLockFile = (zellij + /Cargo.lock);
 
-    naersk-lib = naersk.lib."${system}".override {
-      cargo = rustToolchainToml;
-      rustc = rustToolchainToml;
-    };
+  naersk-lib = naersk.lib."${system}".override {
+    cargo = rustToolchainToml;
+    rustc = rustToolchainToml;
+  };
 
-    RUSTFLAGS = "-Z macro-backtrace";
+  RUSTFLAGS = "-Z macro-backtrace";
 
-    # needs to be a function from list to list
-    cargoOptions = opts: opts ++ [ ];
+  # needs to be a function from list to list
+  cargoOptions = opts: opts ++ [ ];
 
-    # env
-    RUST_BACKTRACE = 1;
+  # env
+  RUST_BACKTRACE = 1;
 
-    targets = [ "wasm32-wasi" ];
-    extensions = [
-      "rust-src"
-      #"rustfmt-preview"
-      "clippy-preview"
-      "rust-analysis"
-    ];
+  targets = [ "wasm32-wasi" ];
+  extensions = [
+    "rust-src"
+    #"rustfmt-preview"
+    "clippy-preview"
+    "rust-analysis"
+  ];
 
-    buildInputs = [
-      #rustNaerskBuild
-      rustToolchainToml
-      pkgs.cargo-make
-      pkgs.rust-analyzer
-      pkgs.mkdocs
-      #binaryenUnstable
+  buildInputs = [
+    #rustNaerskBuild
+    rustToolchainToml
+    pkgs.cargo-make
+    pkgs.rust-analyzer
+    pkgs.mkdocs
+    #binaryenUnstable
 
-      # in order to run tests
-      pkgs.openssl
-      pkgs.pkg-config
-      pkgs.binaryen
+    # in order to run tests
+    pkgs.openssl
+    pkgs.pkg-config
+    pkgs.binaryen
 
-      pkgs.just
-      # formatting
-      pkgs.nixpkgs-fmt
-      pkgs.treefmt
-    ];
+    pkgs.just
+    # formatting
+    pkgs.nixpkgs-fmt
+    pkgs.treefmt
+  ];
 
-  in
-  rec {
-    #`nix build`
-    packages.zellij = naersk-lib.buildPackage {
-      pname = "zellij";
-      root = zellij;
-    };
-    defaultPackage = packages.zellij;
+in
+rec {
+  #`nix build`
+  packages.zellij = naersk-lib.buildPackage {
+    pname = "zellij";
+    root = zellij;
+  };
+  defaultPackage = packages.zellij;
 
-    # `nix run`
-    apps.zellij = flake-utils.lib.mkApp {
-      drv = packages.zellij;
-    };
-    defaultApp = apps.zellij;
+  # `nix run`
+  apps.zellij = flake-utils.lib.mkApp {
+    drv = packages.zellij;
+  };
+  defaultApp = apps.zellij;
 
-    devShell = pkgs.callPackage ./devShell.nix { inherit buildInputs RUST_BACKTRACE CARGO_INSTALL_ROOT; };
-  })
+  devShell = pkgs.callPackage ./devShell.nix { inherit buildInputs RUST_BACKTRACE CARGO_INSTALL_ROOT; };
+})
